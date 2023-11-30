bb33a5a58e018411bc72881c1e52a5546983a300
yaohwu <yaohwu@163.com>
2023-11-30 21:59:36 +0800

    Site updated: 2023-11-30 21:59:36

--- js/src/even.js
+++ js/src/even.js
@@ -5,7 +5,7 @@
     this.config = config;
   }
 
-  Even.prototype.setup = function() {
+  Even.prototype.setup = function () {
     var leancloud = this.config.leancloud;
 
     this.navbar();
@@ -21,7 +21,7 @@
     if (leancloud.app_id && leancloud.app_key) {
       this.recordReadings();
     }
-    if(this.config.latex) {
+    if (this.config.latex) {
       this.renderLaTeX();
     }
     this.backToTop();
@@ -32,10 +32,10 @@
     var $navIcon = $('.mobile-navbar-icon');
 
     var slideout = new Slideout({
-      'panel': document.getElementById('mobile-panel'),
-      'menu': document.getElementById('mobile-menu'),
-      'padding': 180,
-      'tolerance': 70
+      panel: document.getElementById('mobile-panel'),
+      menu: document.getElementById('mobile-menu'),
+      padding: 180,
+      tolerance: 70,
     });
     slideout.disableTouch();
 
@@ -59,8 +59,8 @@
   };
 
   Even.prototype.responsiveTable = function () {
-    var tables = $('.post-content > table')
-    tables.wrap('<div class="table-responsive">')
+    var tables = $('.post-content > table');
+    tables.wrap('<div class="table-responsive">');
   };
 
   Even.prototype.scrollToc = function () {
@@ -73,35 +73,38 @@
       $(window).scroll(function () {
         var tocState = {
           start: {
-            'position': 'absolute',
-            'top': minScrollTop
+            position: 'absolute',
+            top: minScrollTop,
           },
           process: {
-            'position': 'fixed',
-            'top': SPACING
-          }
-        }
+            position: 'fixed',
+            top: SPACING,
+          },
+        };
         var scrollTop = $(window).scrollTop();
         if (scrollTop < minScrollTop) {
           $toc.css(tocState.start);
         } else {
           $toc.css(tocState.process);
-          
-          if($(".post-toc").css("display") != "none"){
+
+          if ($('.post-toc').css('display') != 'none') {
             var maxTocTop = $footer.offset().top - $toc.height() - SPACING;
             var tocCenterThreshold = document.documentElement.scrollTop + window.innerHeight / 2;
-            if ($(".toc-link.active").offset() != undefined && $(".toc-link.active").offset().top > tocCenterThreshold) {
-              var distanceBetween = $(".post-toc").offset().top - $(".toc-link.active").offset().top;
-              $(".post-toc").offset({
-                  top: Math.min(maxTocTop, tocCenterThreshold + distanceBetween),
+            if (
+              $('.toc-link.active').offset() != undefined &&
+              $('.toc-link.active').offset().top > tocCenterThreshold
+            ) {
+              var distanceBetween = $('.post-toc').offset().top - $('.toc-link.active').offset().top;
+              $('.post-toc').offset({
+                top: Math.min(maxTocTop, tocCenterThreshold + distanceBetween),
               });
             }
-            if (maxTocTop < $(".post-toc").offset().top) {
-              $(".post-toc").offset({ top: maxTocTop });
+            if (maxTocTop < $('.post-toc').offset().top) {
+              $('.post-toc').offset({ top: maxTocTop });
             }
           }
         }
-      })
+      });
     }
   };
 
@@ -133,16 +136,18 @@
   Even.prototype.fancybox = function () {
     if ($.fancybox) {
       $('.post').each(function () {
-        $(this).find('img').each(function () {
-          var href = 'href="' + this.src + '"';
-          var title = 'title="' + this.alt + '"';
-          $(this).wrap('<a class="fancybox" ' + href + ' ' + title + '></a>');
-        });
+        $(this)
+          .find('img')
+          .each(function () {
+            var href = 'href="' + this.src + '"';
+            var title = 'title="' + this.alt + '"';
+            $(this).wrap('<a class="fancybox" ' + href + ' ' + title + '></a>');
+          });
       });
 
       $('.fancybox').fancybox({
         openEffect: 'elastic',
-        closeEffect: 'elastic'
+        closeEffect: 'elastic',
       });
     }
   };
@@ -159,7 +164,7 @@
     }
 
     function updateVisits(dom, time) {
-      var readText = dom.text().replace(/(\d+)/i, time)
+      var readText = dom.text().replace(/(\d+)/i, time);
       dom.text(readText);
     }
 
@@ -170,61 +175,69 @@
       var title = $visits.data('title').trim();
 
       query.equalTo('url', url);
-      query.find().then(function (results) {
-        if (results.length > 0) {
-          var counter = results[0];
-          counter.save(null, {
-            fetchWhenSave: true
-          }).then(function (counter) {
-            counter.increment('time', 1);
-            return counter.save();
-          }).then(function (counter) {
-            updateVisits($visits, counter.get('time'));
-          });
-        } else {
-          var newcounter = new Counter();
-          newcounter.set('title', title);
-          newcounter.set('url', url);
-          newcounter.set('time', 1);
+      query.find().then(
+        function (results) {
+          if (results.length > 0) {
+            var counter = results[0];
+            counter
+              .save(null, {
+                fetchWhenSave: true,
+              })
+              .then(function (counter) {
+                counter.increment('time', 1);
+                return counter.save();
+              })
+              .then(function (counter) {
+                updateVisits($visits, counter.get('time'));
+              });
+          } else {
+            var newcounter = new Counter();
+            newcounter.set('title', title);
+            newcounter.set('url', url);
+            newcounter.set('time', 1);
 
-          var acl = new AV.ACL();
-          acl.setWriteAccess('*', true)
-          acl.setReadAccess('*', true)
-          newcounter.setACL(acl)
+            var acl = new AV.ACL();
+            acl.setWriteAccess('*', true);
+            acl.setReadAccess('*', true);
+            newcounter.setACL(acl);
 
-          newcounter.save().then(function () {
-            updateVisits($visits, newcounter.get('time'));
-          });
-        }
-      }, function (error) {
-        // eslint-disable-next-line
-        console.log('Error:' + error.code + ' ' + error.message);
-      });
+            newcounter.save().then(function () {
+              updateVisits($visits, newcounter.get('time'));
+            });
+          }
+        },
+        function (error) {
+          // eslint-disable-next-line
+          console.log('Error:' + error.code + ' ' + error.message);
+        },
+      );
     }
 
     function showTime(Counter) {
       let index = 0;
       $visits.each(function () {
         var $this = $(this);
-        setTimeout(
-          function() {
-            var query = new AV.Query(Counter);
-            var url = $this.data('url').trim();
-    
-            query.equalTo('url', url);
-            query.find().then(function (results) {
+        setTimeout(function () {
+          var query = new AV.Query(Counter);
+          var url = $this.data('url').trim();
+
+          query.equalTo('url', url);
+          query.find().then(
+            function (results) {
               if (results.length === 0) {
                 updateVisits($this, 0);
               } else {
                 var counter = results[0];
                 updateVisits($this, counter.get('time'));
               }
-            }, function (error) {
+            },
+            function (error) {
               // eslint-disable-next-line
               console.log('Error:' + error.code + ' ' + error.message);
-            });
-          }, 100*(index++));     
-      })
+            },
+          );
+        }, 100 * index++);
+      });
     }
   };
 
@@ -246,16 +259,23 @@
 
   Even.prototype.renderLaTeX = function () {
     var loopID = setInterval(function () {
-      if(window.MathJax) {
+      if (window.MathJax) {
         var jax = window.MathJax;
-        jax.Hub.Config({ tex2jax: { inlineMath: [['$', '$'], ['\\(', '\\)']] }});
+        jax.Hub.Config({
+          tex2jax: {
+            inlineMath: [
+              ['$', '$'],
+              ['\\(', '\\)'],
+            ],
+          },
+        });
         jax.Hub.Queue(['Typeset', jax.Hub, $(document.body)[0]]);
         clearInterval(loopID);
       }
     }, 500);
-  }
+  };
 
   var config = window.config;
   var even = new Even(config);
   even.setup();
-}(window))
+})(window);
